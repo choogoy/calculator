9012cfaaaced63853d0ad046de10d03c70f707bc
choogoy <victormm0312@yandex.ru>
2021-04-02 08:25:28 +0300

    обязательное задание

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 'use strict';
 
-let money, 
-    income, 
-    addExpenses, 
-    deposit, 
-    mission, 
-    period;
-    
-alert('Hello World!');
+let money = 90000, // Доход за месяц
+    income = 50000, // дополнительный доходод
+    addExpenses = 'Продукты, бензин, книги', // дополнительные расходы
+    deposit = true, // наличие депозита 
+    mission = 300000, // сумма, которую надо накопить 
+    period = 12, // срок накопления в месяцах
+    budgetDay = money / 30; // дневной бюджет
 
-console.log('calculator');
+console.log('тип данных money: ', typeof money);
+console.log('тип данных income: ', typeof income);
+console.log('тип данных deposit: ', typeof deposit);
+console.log('длина строки addExpenses: ', addExpenses.length);
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} рублей`);
+console.log(addExpenses.toLowerCase().split(', '));
+console.log('budgetDay: ', budgetDay);
